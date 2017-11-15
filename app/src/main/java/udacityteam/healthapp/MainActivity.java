83aa3fd0e373a8d9854f053fa806d4043a6ab359
Viktor Wellbranding <v.vostrikov123@gmail.com>
2017-11-15 23:16:09 +0200

    Beginning

---
+++ app/src/main/java/udacityteam/healthapp/MainActivity.java
@@ -0,0 +1,13 @@
+package udacityteam.healthapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
